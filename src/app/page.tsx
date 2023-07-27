8e5d5d880c5e899f8be784a0018d1465c38c9514
elduke717 <spacematcha@proton.me>
2023-07-27 16:31:10 -0400

    update readme, add some public images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src="/images/lo--2.jpg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}

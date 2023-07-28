ad88a289f6d6a7fe492f683e5b4dbaa162c29b36
elduke717 <spacematcha@proton.me>
2023-07-28 17:13:41 -0400

    add client components like header and headerlink to experiment with

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import HeaderLink from './HeaderLink';
+const Header = () => {
+    return (
+        <div>
+        <h1>Header</h1>
+        <HeaderLink />
+        </div>
+    );
+    }
+    
+export default Header;

c96eca752b62cfa92d8022ae17b30822fcbbf177
elduke717 <spacematcha@proton.me>
2023-07-31 21:14:56 -0400

    style links to page views

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <h1 className="font-bold text-4xl">Nick Huemmer</h1>
+      <h1 className="font-bold text-4xl mx-5 my-8">Nick Huemmer</h1>
+      <div className='nx-flex nx-grow nx-flex-wrap nx-items-center nx-justify-end nx-gap-3'>
+        <a className="nx-cursor-default dark:nx-text-gray-400 nx-text-gray-600"href="/">Nick Huemmer</a>
+        <a href="/software">software</a>
+      </div>
         <div>
           {children}
         </div>

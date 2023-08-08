d0de4f260c2371cde7951b953b7739f1f221c9df
elduke717 <spacematcha@proton.me>
2023-08-07 23:16:34 -0400

    add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,10 +20,8 @@ export default function Home() {
       <div className="flex flex-col items-left justify-center mx-10">
       <p className='my-5'>
         I&#39;m a Software Engineer with a focus on frontend and fullstack development. 
-        I use JavaScript, React, HTML5, CSS, Node.js, Express.js, Next.js, Astro, SvelteKit, and TypeScript for my work.</p>  
+        I am proficient with JavaScript, React, HTML5, CSS, Node.js, Express.js, Next.js, Astro, SvelteKit, and TypeScript.  I enjoy crafting interactive, functional, accessible and aesthetically appealing websites and applications</p>  
         <p className='my-5'>I also have experience contributing to several open source projects including Reactime, a performance monitoring tool for React applications.
-        </p>
-        <p className='my-5'>I enjoy crafting interactive, functional, accessible and aesthetically appealing websites and applications. 
         </p>
         <p className='my-5'>  
         My blog about software engineering and related topics at can be found at <a className="text-lg font-bold decoration-solid"href="https://www.nickhuemmer.com">nickhuemmer.com</a>.</p>
@@ -34,11 +32,11 @@ export default function Home() {
       <a className="text-lg font-semibold"href="mailto:hello@nickhuemmer.me">hello@nickhuemmer.me</a>
       </div>
       <br />
-      <hr className="w-16 border-t-2 border-gray-400 mb-4" />
+      <hr className="w-48 border-t-2 border-gray-400 mb-4" />
       <div className="logos flex flex-row items-center justify-center space-x-4">
   <div className='logo-div social flex flex-row items-center justify-center'>
     
-    <a href="https://twitter.com/njhuemmer">
+    <a className="mx-5" href="https://twitter.com/njhuemmer">
       <Image
         src="/logos/twitter250.svg"
         alt="twitter logo"
@@ -48,7 +46,7 @@ export default function Home() {
         className=""
       />
     </a>
-    <a href="https://github.com/ElDuke717">
+    <a className="mx-5" href="https://github.com/ElDuke717">
       <Image
         src="/logos/github250.svg"
         alt="github logo"
@@ -58,7 +56,7 @@ export default function Home() {
         className="next-image logo"
       />
     </a>
-    <a href="https://www.linkedin.com/in/nickjhuemmer/">
+    <a className="mx-5" href="https://www.linkedin.com/in/nickjhuemmer/">
       <Image
         src="/logos/linkedin250.svg"
         alt="linkedin logo"
@@ -68,7 +66,7 @@ export default function Home() {
         className="next-image logo"
       />
     </a>
-    <a href="https://medium.com/@njhuemmer">
+    <a className="mx-5" href="https://medium.com/@njhuemmer">
       <Image
         src="/logos/medium250-2.png"
         alt="medium logo"

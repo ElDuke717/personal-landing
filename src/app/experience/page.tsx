819d90b95ce49c3a766bc354ba975b3d87770dad
elduke717 <spacematcha@proton.me>
2023-08-21 21:39:20 -0400

    style changes made to experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,109 +1,183 @@
 export default function Experience() {
   return (
     <div className="max-w-3xl mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">NICK HUEMMER</h1>
+      <h1 className="text-3xl font-bold mb-4">EXPERIENCE</h1>
       <p className="mb-2">
-        <a className="text-blue-500 hover:underline" href="https://www.linkedin.com/in/nickjhuemmer">
-          linkedin.com/in/nickjhuemmer
-        </a>{' '}
-        | ElDuke717 |{' '}
-        <a className="text-blue-500 hover:underline" href="https://www.nickhuemmer.me">
-          nickhuemmer.me
+        <a
+          className="text-blue-500 hover:underline"
+          href="https://www.linkedin.com/in/nickjhuemmer"
+        >
+          LinkedIn
+        </a>{" "}
+        |
+        <a
+          className="text-blue-500 hover:underline"
+          href="https://github.com/ElDuke717"
+        >
+          {" "}
+          GitHub{" "}
         </a>
       </p>
-      <p className="mb-4">Software Engineer Richmond, VA 23832 | 224-420-2364 | njhuemmer@protonmail.com</p>
 
       <h2 className="text-xl font-bold mb-2">SKILLS:</h2>
       <div className="mb-4">
-        <strong>Strong:</strong> JavaScript (ES6+), React, HTML5, CSS, Sass, Node.js, Express.js, REST API, Postman, Axios, Linux
+        <strong>Strong:</strong> JavaScript (ES6+), React, HTML5, CSS, Sass,
+        Node.js, Express.js, REST API, Postman, Axios, Linux
       </div>
       <div className="mb-4">
-        <strong>Experienced:</strong> Next.js, Redux, Astro, Svelte, SvelteKit, TypeScript, TailwindCSS, Bootstrap, Testing (Jest, Mocha),
-        Open AI API, PostgreSQL, NoSQL(MongoDB), Docker, Webpack, Visx, D3.js, YAML, Babel, Redux, Pug, leading teams, Asana, Figma
+        <strong>Experienced:</strong> Next.js, Redux, Astro, Svelte, SvelteKit,
+        TypeScript, TailwindCSS, Bootstrap, Testing (Jest, Mocha), Open AI API,
+        PostgreSQL, NoSQL(MongoDB), Docker, Webpack, Visx, D3.js, YAML, Babel,
+        Redux, Pug, leading teams, Asana, Figma
       </div>
 
       <h2 className="text-xl font-bold mb-2">SOFTWARE EXPERIENCE:</h2>
       <p className="mb-2">
-        Software Engineer | OSLabs | Reactime | Open Source Chrome developer tool for debugging and performance monitoring in React
-        applications. 2023
+        Software Engineer | OSLabs | Reactime | Open Source Chrome developer
+        tool for debugging and performance monitoring in React applications.
+        2023
       </p>
       <ul className="list-disc list-inside mb-4">
         <li>
-          Implemented TypeScript to identify and resolve errors at compile-time, resulting in a 20% increase in application stability.
+          Implemented TypeScript to identify and resolve errors at compile-time,
+          resulting in a 20% increase in application stability.
         </li>
         <li>
-          Incorporated Visx and D3 to create interactive, developer-friendly radial bar graphs that illustrate React performance during
+          Incorporated Visx and D3 to create interactive, developer-friendly
+          radial bar graphs that illustrate React performance during
           development, improving usability and accessibility.
         </li>
-        <li>Elevated product stability and performance by increasing Jest testing coverage to over 80% of the component code.</li>
-        <li>Led the Initiative to resolve critical bugs in Reactime by updating outdated modules and code.</li>
-        <li>Product revitalized under tech accelerator OS Labs (<a className="text-blue-500 hover:underline" href="https://www.opensourcelabs.io">www.opensourcelabs.io</a>).</li>
+        <li>
+          Elevated product stability and performance by increasing Jest testing
+          coverage to over 80% of the component code.
+        </li>
+        <li>
+          Led the Initiative to resolve critical bugs in Reactime by updating
+          outdated modules and code.
+        </li>
+        <li>
+          Product revitalized under tech accelerator OS Labs (
+          <a
+            className="text-blue-500 hover:underline"
+            href="https://www.opensourcelabs.io"
+          >
+            www.opensourcelabs.io
+          </a>
+          ).
+        </li>
+      </ul>
+
+      <p className="mb-2">Software Developer| Goal Keeper | Goal Tracker App</p>
+      <ul className="list-disc list-inside mb-4">
+        <li>
+          Implemented React library to construct a responsive front-end with UI
+          components to collect and store user goals.
+        </li>
+        <li>
+          Deployed PostgreSQL for data storage achieving a 15% reduction in
+          query response time and refined overall product performance.
+        </li>
       </ul>
 
       <p className="mb-2">
-        Software Developer| Goal Keeper | Goal Tracker App
+        Software Developer | StyleGPT | AI - assisted styling tool for
+        developers
       </p>
       <ul className="list-disc list-inside mb-4">
-        <li>Implemented React library to construct a responsive front-end with UI components to collect and store user goals.</li>
-        <li>Deployed PostgreSQL for data storage achieving a 15% reduction in query response time and refined overall product performance.</li>
-      </ul>
-
-      <p className="mb-2">
-        Software Developer | StyleGPT | AI - assisted styling tool for developers
-      </p>
-      <ul className="list-disc list-inside mb-4">
-        <li>Connected OpenAI API for CSS styles based on user input, enhancing UI creation.</li>
-        <li>Enhanced data exchange with Axios, lowering response times by 30% for operational efficiency.</li>
+        <li>
+          Connected OpenAI API for CSS styles based on user input, enhancing UI
+          creation.
+        </li>
+        <li>
+          Enhanced data exchange with Axios, lowering response times by 30% for
+          operational efficiency.
+        </li>
       </ul>
 
       <h2 className="text-xl font-bold mb-2">CAREER EXPERIENCE:</h2>
       <p className="mb-1">
-        Health Coach and Personal Trainer | Self Employed | Helped middle-aged men lose weight and improve health-status
+        Health Coach and Personal Trainer | Self Employed | Helped middle-aged
+        men lose weight and improve health-status
       </p>
       <p className="mb-1">2019-2021</p>
       <ul className="list-disc list-inside mb-4">
-        <li>Built coaching business including web and social media presence, with greater than 250 followers.</li>
         <li>
-          Created nutritional guidance and exercise programs that built baseline strength and improved mobility and facilitated weight
-          loss of greater than 25 lbs for all clients.
+          Built coaching business including web and social media presence, with
+          greater than 250 followers.
+        </li>
+        <li>
+          Created nutritional guidance and exercise programs that built baseline
+          strength and improved mobility and facilitated weight loss of greater
+          than 25 lbs for all clients.
         </li>
       </ul>
 
       <p className="mb-1">
-        Senior Product Developer | Clif Bar | Product creation, formulation, optimization and commercialization
+        Senior Product Developer | Clif Bar | Product creation, formulation,
+        optimization and commercialization
       </p>
       <p className="mb-1">2014-2019</p>
       <ul className="list-disc list-inside mb-4">
-        <li>Created lab notebooks using Excel and Sharepoint, reducing formula development time by 30%.</li>
-        <li>Formulated products for improved nutrition and saved up to $2 million yearly through process optimizations.</li>
+        <li>
+          Created lab notebooks using Excel and Sharepoint, reducing formula
+          development time by 30%.
+        </li>
+        <li>
+          Formulated products for improved nutrition and saved up to $2 million
+          yearly through process optimizations.
+        </li>
       </ul>
 
-      <p className="mb-1">Scientist II | Clorox | Product development for Hidden Valley Brand</p>
+      <p className="mb-1">
+        Scientist II | Clorox | Product development for Hidden Valley Brand
+      </p>
       <p className="mb-1">2012-2014</p>
       <ul className="list-disc list-inside mb-4">
         <li>
-          Developed and facilitated commercialization and launch of salad kits for the Hidden Valley Brand, the largest product launch
-          in 2013.
+          Developed and facilitated commercialization and launch of salad kits
+          for the Hidden Valley Brand, the largest product launch in 2013.
         </li>
-        <li>Led new product development initiative leading to five new products to fill innovation pipeline.</li>
+        <li>
+          Led new product development initiative leading to five new products to
+          fill innovation pipeline.
+        </li>
       </ul>
 
-      <p className="mb-1">Product Application Technologist | Nestle | Product development for Pizza Division</p>
+      <p className="mb-1">
+        Product Application Technologist | Nestle | Product development for
+        Pizza Division
+      </p>
       <p className="mb-1">2010-2012</p>
       <ul className="list-disc list-inside mb-4">
-        <li>Managed and led product development including timeline management, cost control, ingredient procurement, and leveraging supplier capabilities.</li>
-        <li>Collaborated with marketing, operations, and finance to deliver novel pizza products.</li>
+        <li>
+          Managed and led product development including timeline management,
+          cost control, ingredient procurement, and leveraging supplier
+          capabilities.
+        </li>
+        <li>
+          Collaborated with marketing, operations, and finance to deliver novel
+          pizza products.
+        </li>
       </ul>
 
-      <p className="mb-1">Scientist | Kraft Foods | Product development for Spoonable and Pourable Dressings</p>
+      <p className="mb-1">
+        Scientist | Kraft Foods | Product development for Spoonable and Pourable
+        Dressings
+      </p>
       <p className="mb-1">2007-2010</p>
       <ul className="list-disc list-inside mb-4">
-        <li>Generated over $1M of productivity savings by managing raw material costs and factoring efficiencies into processes.</li>
+        <li>
+          Generated over $1M of productivity savings by managing raw material
+          costs and factoring efficiencies into processes.
+        </li>
       </ul>
 
       <h2 className="text-xl font-bold mb-2">EDUCATION:</h2>
       <p>Codesmith Advanced Software Engineering Immersion | Resident</p>
-      <p>North Carolina State University | BS Biological Sciences, Food Science and Nutrition</p>
+      <p>
+        North Carolina State University | BS Biological Sciences, Food Science
+        and Nutrition
+      </p>
       <p>The Ohio State University | MS Food Science</p>
 
       <h2 className="text-xl font-bold mb-2">TALKS AND PUBLICATIONS:</h2>

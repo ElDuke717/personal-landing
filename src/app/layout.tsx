2ef7381302a02e0f302e43e3fbf42696dac292ab
elduke717 <spacematcha@proton.me>
2023-09-11 11:12:45 -0400

    change the format back since heading and navbar were repeating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,39 +12,40 @@ export const metadata: Metadata = {
   description: "Nick Huemmer is a software engineer.",
 };
 // Children is the content of the page. They must added to the layout.
-export default function RootLayout({ children }: { children: React.ReactNode; }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 className="font-bold text-2xl md:text-4xl mt-[75px]">
-            <a href="/">Nick Huemmer</a>
-          </h1>
-          <div className="flex justify-start items-center my-8 text-xl">
-            <a
-              className="cursor-pointer hover:underline hover:text-[#FFFF00] transition duration-1000"
-              href="/"
-            >
-              about
-            </a>
-            <a
-              className="ml-4 cursor-pointer hover:underline hover:text-[#FFA500] transition duration-1000"
-              href="/software"
-            >
-              software
-            </a>
-            <a
-              className="ml-4 cursor-pointer hover:underline hover:text-[#7FFF00] transition duration-1000"
-              href="/experience"
-            >
-              experience
-            </a>
-          </div>
-          <div>{children}</div>
+        <h1 className="font-bold text-2xl md:text-4xl mx-[75px] mt-[75px]">
+          <a href="/">Nick Huemmer</a>
+        </h1>
+        <div className="flex justify-start items-center ml-[75px] my-8 text-xl">
+          <a
+            className="cursor-pointer hover:underline hover:text-[#FFFF00] transition duration-1000 "
+            href="/"
+          >
+            about
+          </a>
+          <a
+            className="ml-4 cursor-pointer hover:underline hover:text-[#FFA500] transition duration-1000"
+            href="/software"
+          >
+            software
+          </a>
+          <a
+            className="ml-4 cursor-pointer hover:underline hover:text-[#7FFF00] transition duration-1000"
+            href="/experience"
+          >
+            experience
+          </a>
         </div>
+        <div>{children}</div>
         <Analytics />
       </body>
     </html>
   );
 }
-
